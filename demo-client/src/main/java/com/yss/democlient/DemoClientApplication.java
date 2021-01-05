3e8a306fbc3bd4bd595e743ff5b6d1c0b82e260d
shiwei1 <shiwei1@ysstech.com>
2021-01-05 17:13:24 +0800

    demo-client

---
+++ demo-client/src/main/java/com/yss/democlient/DemoClientApplication.java
@@ -0,0 +1,20 @@
+package com.yss.democlient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shiwei
+ * @date 2020/12/22
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoClientApplication.class, args);
+    }
+
+}
---
+++ demo-client/src/main/java/com/yss/democlient/controller/DcCntorller.java
@@ -0,0 +1,27 @@
+package com.yss.democlient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shiwei
+ * @date 2020/12/22
+ */
+
+@RestController
+public class DcCntorller {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services:" + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+
+}
