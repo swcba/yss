31d2c2a7dcfd44cf9afc49c5c16f8ed5c93e6b38
shiwei1 <shiwei1@ysstech.com>
2021-01-05 17:20:51 +0800

    demo-config

---
+++ demo-config/src/test/java/com/yss/democonfig/DemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yss.democonfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
