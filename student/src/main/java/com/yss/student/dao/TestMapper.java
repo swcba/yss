c9f9666d18ec009f70679af4e9568b6897e22838
shiwei1 <shiwei1@ysstech.com>
2021-01-06 09:15:02 +0800

    测试Mapper

---
+++ student/src/main/java/com/yss/student/dao/TestMapper.java
@@ -0,0 +1,23 @@
+package com.yss.student.dao;/****************************************************
+ * 创建人：     @author shiwei1    
+ * 创建时间: 2021/1/6/9:13
+ * 项目名称：  demo-thymeleaf
+ * 文件名称: null.java
+ * 文件描述: @Description: (用一句话描述该文件做什么)
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2021
+ *
+ ********************************************************/
+
+
+/**
+ * 包名称：com.yss.student.dao
+ * 类名称：TestMapper
+ * 类描述：
+ * 创建人：@author shiwei1
+ * 创建时间：2021/1/6/9:13
+ */
+
+public class TestMapper {
+}
